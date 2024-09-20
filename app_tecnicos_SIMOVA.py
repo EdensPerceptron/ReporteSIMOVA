0b5e42138ae0df97941d04da3476b8d9e01c1b7c
Pedro Luis Alcazar Cruz <121834311+EdensPerceptron@users.noreply.github.com>
2024-09-20 18:10:35 -0400

    Update app_tecnicos_SIMOVA.py

--- app_tecnicos_SIMOVA.py
+++ app_tecnicos_SIMOVA.py
@@ -2,7 +2,14 @@ import pandas as pd
 import streamlit as st
 from datetime import time
 import plotly.express as px
+import plotly.graph_objs as go
 import numpy as np
+import matplotlib.pyplot as plt  # Importar matplotlib
+import seaborn as sns
+import locale
+
+# Configurar el idioma a español
+locale.setlocale(locale.LC_TIME, 'Spanish_Spain') 
 
 st.set_page_config(
     page_title='Reporte SIMOVA',
@@ -60,10 +67,10 @@ diferencia_dias = ultimo_dia - primer_dia
 # Crear un rango de fechas entre el primer y el último día
 rango_fechas = pd.date_range(start=primer_dia, end=ultimo_dia)
 # Filtrar los días que no sean domingo (el día 6 en pandas corresponde al domingo)
-dias_no_domingo = len(rango_fechas[rango_fechas.weekday != 6])
+dias_no_domingo = len(rango_fechas)
 
 st.text(f'Fecha del Reporte: Entre {primer_dia} y {ultimo_dia}')
-st.text(f'días laborales: {dias_no_domingo}')
+st.text(f'días de registro: {dias_no_domingo }')
 st.text(f'Cantidad de técnicos: {cantidad_tecnicos}')
 
 
@@ -76,7 +83,7 @@ grupoxtecnico_df = df.groupby(['Nome Técnico']).agg({
 }).reset_index()
 
 grupoxtecnico_df.iloc[:, 1:] = grupoxtecnico_df.iloc[:, 1:].apply(pd.to_numeric, errors='coerce')
-grupoxtecnico_df.iloc[:, 1:] = grupoxtecnico_df.iloc[:, 1:] / dias_no_domingo
+grupoxtecnico_df.iloc[:, 1:] = grupoxtecnico_df.iloc[:, 1:]
 
 
 # Agrupar los datos por fecha y técnico
@@ -89,86 +96,102 @@ grouped_df = df.groupby(['Fecha', 'Nome Técnico']).agg({
 
 
 # Selector para el indicador, con 'Calificacion General' como opción por defecto
-indicator = st.selectbox(
-    "Selecciona el indicador a visualizar:",
-    ['Cumple Ingreso a tiempo', 'Cumple Menos de una hora improd.', 'Cumple Salida Correcta', 'Calificacion General'],
-    index=3  
-)
+#indicator = st.selectbox(
+#    "Selecciona el indicador a visualizar:",
+#    ['Cumple Ingreso a tiempo', 'Cumple Menos de una hora improd.', 'Cumple Salida Correcta', 'Calificacion General'],
+#    index=3  
+#)
 
-# Ordenar el DataFrame por el indicador seleccionado, de mayor a menor
-grupoxtecnico_df_sorted = grupoxtecnico_df.sort_values(by=indicator, ascending=False)
 
-# Crear gráfico de barras utilizando Plotly
-fig = px.bar(
-    grupoxtecnico_df_sorted,  # Usar el DataFrame ordenado
-    x='Nome Técnico',         # Eje x es el nombre del técnico
-    y=indicator,              # Eje y es el indicador seleccionado
-    title=f'{indicator} por Técnico',  # Título del gráfico
-    labels={'Nome Técnico': 'Nombre del Técnico', indicator: indicator},  # Etiquetas
-    color=indicator,          # Colorear las barras según el indicador
-    color_continuous_scale=px.colors.sequential.Aggrnyl  # Paleta de colores (puedes cambiarla)
-)
+indicator1 ='Cumple Ingreso a tiempo'
+indicator2='Cumple Menos de una hora improd.'
+indicator3='Cumple Salida Correcta'
+indicator4='Calificacion General'
 
-# Personalizar el gráfico
-fig.update_layout(
-    xaxis_title='Nombre del Técnico',
-    yaxis_title=indicator,
-    title={'x': 0.5},  # Centrar el título
-    template='plotly_white'  # Usar un tema más limpio
-)
+def graficos(indicator):
+    # Ordenar el DataFrame por el indicador seleccionado, de mayor a menor
+    grupoxtecnico_df_sorted = grupoxtecnico_df.sort_values(by=indicator, ascending=False)
 
-# Mostrar gráfico en Streamlit
-st.plotly_chart(fig)
-
-# Colores de John Deere
-john_deere_colors = ['#FD3500', '#367C2B']  # Naranja y verde oscuro
-
-# Crear una tabla de contingencia
-heatmap_data = df.pivot_table(index='Nome Técnico', columns='Fecha', values=indicator, fill_value=0)
-
-# Calcular el valor total del indicador por técnico
-tech_order = heatmap_data.sum(axis=1).sort_values(ascending=False).index
-
-# Reordenar el DataFrame según el valor total del indicador
-heatmap_data = heatmap_data.reindex(index=tech_order)
-
-# Asegúrate de que las columnas de fechas son del tipo datetime
-heatmap_data.columns = pd.to_datetime(heatmap_data.columns)
-
-# Crear un mapa de calor
-fig = px.imshow(
-    heatmap_data,
-    labels=dict(x="Fecha", y="Nome Técnico", color=indicator),
-    color_continuous_scale=[(0, john_deere_colors[0]), (1, john_deere_colors[1])],  # Colores de John Deere
-    aspect="auto"
-)
-
-# Personalizar el gráfico
-fig.update_layout(
-    title=f'Mapa de {indicator} por Técnico y Fecha',
-    xaxis_title='Fecha',
-    yaxis_title='Nombre Técnico',
-    xaxis=dict(
-        tickmode='array',
-        tickvals=list(range(len(heatmap_data.columns))),
-        ticktext=heatmap_data.columns.strftime('%d-%m-%Y'),  # Formato más claro
-        tickangle=45,  # Ángulo de las etiquetas
-        title_font=dict(size=14),  # Tamaño de fuente del título del eje x
-        tickfont=dict(size=10, color='red')  # Tamaño de fuente de las etiquetas
-    ),
-    yaxis=dict(
-        tickmode='array',
-        tickvals=list(range(len(heatmap_data.index))),
-        ticktext=heatmap_data.index,  # Técnicos ordenados
-        title_font=dict(size=14),  # Tamaño de fuente del título del eje y
-        tickfont=dict(size=10)  # Tamaño de fuente de las etiquetas
-    ),
-    coloraxis_colorbar=dict(
-        title=indicator,
-        tickvals=[0, 1],
-        ticktext=["No Cumple", "Cumple"]
+    # Crear gráfico de barras utilizando Plotly
+    fig = px.bar(
+        grupoxtecnico_df_sorted,  # Usar el DataFrame ordenado
+        x='Nome Técnico',         # Eje x es el nombre del técnico
+        y=indicator,              # Eje y es el indicador seleccionado
+        title=f'{indicator} por Técnico',  # Título del gráfico
+        labels={'Nome Técnico': 'Nombre del Técnico', indicator: indicator},  # Etiquetas
+        color=indicator,          # Colorear las barras según el indicador
+        color_continuous_scale=px.colors.sequential.Aggrnyl  # Paleta de colores
     )
-)
 
-# Mostrar el mapa de calor en Streamlit
-st.plotly_chart(fig)
+    # Personalizar el gráfico de barras
+    fig.update_layout(
+        xaxis_title='Nombre del Técnico',
+        yaxis_title=indicator,
+        template='plotly_white'  # Usar un tema más limpio
+    )
+
+    # Colores de John Deere
+    john_deere_colors = ['#FD3500', '#367C2B']  # Naranja y verde oscuro
+
+    # Crear una tabla de contingencia (heatmap_data)
+    heatmap_data = df.pivot_table(index='Nome Técnico', columns='Fecha', values=indicator, fill_value=0)
+
+    # Calcular el valor total del indicador por técnico
+    tech_order = heatmap_data.sum(axis=1).sort_values(ascending=True).index
+
+    # Reordenar el DataFrame según el valor total del indicador
+    heatmap_data = heatmap_data.reindex(index=tech_order)
+
+    # Asegúrate de que las columnas de fechas son del tipo datetime
+    heatmap_data.columns = pd.to_datetime(heatmap_data.columns)
+
+    # Convertir las fechas a formato de cadena con día de la semana en español
+    heatmap_data.columns = heatmap_data.columns.strftime('%A %d-%m-%Y')  # Ej: lunes 01-01-2024
+
+    # Crear el heatmap con Plotly
+    heatmap_fig = go.Figure(
+        data=go.Heatmap(
+            z=heatmap_data.values,  # Los valores del heatmap
+            x=heatmap_data.columns,  # Etiquetas del eje X (fechas)
+            y=heatmap_data.index,    # Etiquetas del eje Y (nombres de técnicos)
+            colorscale=john_deere_colors,  # Aplicar los colores personalizados
+            text=heatmap_data.values,  # Mostrar los valores dentro de las celdas
+            hoverinfo='text',         # Información de hover al pasar sobre la celda
+            showscale=True,           # Mostrar la barra de colores
+            zmin=heatmap_data.min().min(),  # Definir el valor mínimo del color
+            zmax=heatmap_data.max().max(),  # Definir el valor máximo del color
+        )
+    )
+
+    # Simular espaciado entre celdas (agregar bordes blancos finos)
+    heatmap_fig.update_traces(
+        hoverongaps=False,  # Evitar mostrar información en celdas sin valor
+        xgap=1,  # Espaciado horizontal entre celdas
+        ygap=1   # Espaciado vertical entre celdas
+    )
+
+    # Personalizar el layout del heatmap
+    heatmap_fig.update_layout(
+        title=dict(text=f'Cumplimiento de {indicator} por Fecha'),
+        xaxis_title='Fecha',
+        yaxis_title='Nombre Técnico',
+    )
+
+    return fig, heatmap_fig
+
+# Llamar a la función y mostrar los gráficos en Streamlit
+fig, heatmap_fig = graficos(indicator1)
+#st.plotly_chart(fig)
+st.plotly_chart(heatmap_fig)
+
+fig2, heatmap_fig2 = graficos(indicator2)
+#st.plotly_chart(fig2)
+st.plotly_chart(heatmap_fig2)
+
+fig3, heatmap_fig3 = graficos(indicator3)
+#st.plotly_chart(fig3)
+st.plotly_chart(heatmap_fig3)
+
+fig4, heatmap_fig4 = graficos(indicator4)
+#st.plotly_chart(fig4)
+st.plotly_chart(heatmap_fig4)
